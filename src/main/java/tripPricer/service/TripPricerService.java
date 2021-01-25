bcbd41c96b660db075214afd30491a76115f280d
Alexandre Dubois <59750999+ALEXD2758@users.noreply.github.com>
2021-01-25 15:43:31 +0100

    JavaDoc

--- src/main/java/tripPricer/service/TripPricerService.java
+++ src/main/java/tripPricer/service/TripPricerService.java
@@ -15,7 +15,17 @@ public class TripPricerService {
     private Logger logger = LoggerFactory.getLogger(TripPricerService.class);
     private TripPricer tripPricer = new TripPricer();
 
-
+    /**
+     * Get a list of providers based on user preferences
+     *
+     * @param apiKey a string of the key needed, not used at the moment in final method getProviderName
+     * @param attractionId the UUID of the attractionId in a form of a string
+     * @param adults int of the number of adults
+     * @param children int of the number of children
+     * @param nightsStay int of the number of nights stay
+     * @param rewardsPoints int of the reward points
+     * @return a list of providers based on user preferences
+     */
     public List<Provider> getPriceTripPricer(String apiKey, UUID attractionId, int adults, int children,
                                              int nightsStay,
                                              int rewardsPoints) {
